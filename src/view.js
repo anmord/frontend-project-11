6455ad6dab8cc0bdc21f2a5499b1f1c152bded7f
Anton Tarakin <anton_tarakin@mail.ru>
2026-01-04 13:26:47 +0300

    Step 4, fix ci_2

--- src/view.js
+++ src/view.js
@@ -1,3 +1,16 @@
+import i18next from 'i18next';
+
+export const renderError = (input, feedback, errorCode) => {
+  if (!errorCode) {
+    input.classList.remove('is-invalid');
+    feedback.textContent = '';
+    return;
+  }
+
+  input.classList.add('is-invalid');
+  feedback.textContent = i18next.t(errorCode);
+};
+
 export const renderFeeds = (feeds) => {
   const container = document.querySelector('.feeds');
   container.innerHTML = '';
