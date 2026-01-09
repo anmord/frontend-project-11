3c3c27db0ed944f92619f445eab8373a41fe5384
Anton Tarakin <anton_tarakin@mail.ru>
2026-01-09 22:22:57 +0300

    Step 7, fix linter7

--- eslint.config.js
+++ eslint.config.js
@@ -1,53 +1,17 @@
+import js from '@eslint/js'
 import globals from 'globals'
-import importPlugin from 'eslint-plugin-import'
+import { defineConfig } from 'eslint/config'
+import { includeIgnoreFile } from '@eslint/compat'
+import stylistic from '@stylistic/eslint-plugin'
+import { fileURLToPath } from 'url'
 
-export default [
-  {
-    ignores: ['dist/', 'node_modules/'],
-  },
-  {
-    languageOptions: {
-      globals: {
-        ...globals.node,
-        ...globals.jest,
-        ...globals.browser,
-        bootstrap: 'readonly',
-      },
-      parserOptions: {
-        ecmaVersion: 'latest',
-        sourceType: 'module',
-      },
-    },
-    plugins: {
-      import: importPlugin,
-    },
-    rules: {
-      'arrow-parens': 'off',
-      'brace-style': 'off',
-      'no-trailing-spaces': 'off',
-      '@stylistic/no-multi-spaces': 'off',
-      '@stylistic/brace-style': 'off',
-      '@stylistic/no-trailing-spaces': 'off',
-      'no-multiple-empty-lines': 'off',
+const gitIgnorePath = fileURLToPath(new URL('.gitignore', import.meta.url))
+const eslintIgnorePath = fileURLToPath(new URL('.eslintignore', import.meta.url))
 
-      // Логика кода
-      'semi': ['error', 'never'],
-      'no-underscore-dangle': [
-        'error',
-        { allow: ['__filename', '__dirname'] },
-      ],
-      'import/extensions': 'off',
-      'import/no-named-as-default': 'off',
-      'import/no-named-as-default-member': 'off',
-      'import/no-extraneous-dependencies': 'off',
-      'no-console': 'off',
-      'eol-last': 'off',
-    },
-  },
-  {
-    files: ['postcss.config.cjs'],
-    rules: {
-      'global-require': 'off',
-    },
-  },
-]
+export default defineConfig([
+  includeIgnoreFile(gitIgnorePath),
+  includeIgnoreFile(eslintIgnorePath),
+  stylistic.configs.recommended,
+  { files: ['**/*.{js,mjs,cjs}'], plugins: { js }, extends: ['js/recommended'] },
+  { files: ['**/*.{js,mjs,cjs}'], languageOptions: { globals: { ...globals.browser, process: 'readonly' } } },
+])
