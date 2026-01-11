7fa51c74a9d374ddb9e2d35d00c1c6114ae004e8
Anton Tarakin <anton_tarakin@mail.ru>
2026-01-11 13:20:45 +0300

    Moving initialization to init.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,3 @@
 // @ts-check
 import init from './init.js'
-import initI18n from './i18n.js'
-initI18n().then(() => {
-  init()
-})
+init()
--- src/init.js
+++ src/init.js
@@ -1,19 +1,24 @@
 // @ts-check
+import initI18n from './i18n.js'
 import app from './app.js'
 export default () => {
-  const state = {
-    feeds: [],
-    posts: [],
-    readPosts: new Set(),
-    form: {
-      error: null,
-      status: 'idle',
-    },
-  }
-  const elements = {
-    form: document.querySelector('.rss-form'),
-    input: document.querySelector('#url-input'),
-    feedback: document.querySelector('.feedback'),
-  }
-  app(state, elements)
+  initI18n().then(() => {
+    const state = {
+      feeds: [],
+      posts: [],
+      readPosts: new Set(),
+      form: {
+        error: null,
+        status: 'idle',
+      },
+    }
+
+    const elements = {
+      form: document.querySelector('.rss-form'),
+      input: document.querySelector('#url-input'),
+      feedback: document.querySelector('.feedback'),
+    }
+
+    app(state, elements)
+  })
 }
